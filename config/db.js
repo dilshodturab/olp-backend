b73728bf3f34c647335afb80f20c359a76571d6d
dilshodturab <avlomberdioglid@gmail.com>
2025-10-04 22:39:13 +0500

    env file change

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const localConfig = {
-    user: 'olpuser',
-    host: 'localhost',
-    database: 'olpdb',
-    password: 'olppassword',
-    port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 };
 
 const productionConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-}
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : localConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? productionConfig : localConfig,
+);
 
 module.exports = { pool };
